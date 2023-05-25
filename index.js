22cdc406cfe94a8114296a78c39b0f670d2edd65
Subham Chanda <134305638+dev-subhamchanda@users.noreply.github.com>
2023-05-25 12:04:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+//Targets and variables
+const searchBox = document.querySelector("#searchbox");
+const searchBtn = document.querySelector(".searchBtn");
+const currentCity = document.querySelector(".currentCity");
+let temperature = document.querySelector(".temperature");
+let windSpeed = document.querySelector(".windSpeed");
+let humidity = document.querySelector(".humidity");
+let feelsLike = document.querySelector(".feelsLike");
+let city = searchBox.value
+//functions
+const getCity = () => {
+//   e.preventDefault();
+  city = searchBox.value.charAt(0).toUpperCase() + searchBox.value.substr(1 , searchBox.value.length)
+  currentCity.innerHTML = city
+  fetchWeather(city)
+  console.log(city)
+  searchBox.value=''
+};
+
+//Events
+searchBtn.addEventListener("click", getCity);
+
+//Weather API
+const fetchWeather = async (city) => {
+  const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "53bd57b6b9mshaa86442cf6439e1p184fa1jsnc3fb37d4481e",
+      "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+	
+	//Parsing result to object which was string earlier
+	let weatherObj = JSON.parse(result)
+	console.log(weatherObj) //displaying object
+	//customizing display of weather
+	temperature.innerHTML = `Temperature: ${weatherObj.temp}`
+	windSpeed.innerHTML = `Wind Speed: ${weatherObj.wind_speed}`
+	humidity.innerHTML = `Humidity: ${weatherObj.humidity}`
+	feelsLike.innerHTML = `Feels Like: ${weatherObj.feels_like}`
+  } catch (error) {
+	alert('error')
+  }
+};
+
+getCity()
+
+
